ed6c98e6e370b949a25555f1fb55a77c18e06ee9
anubhavmohandas <anubhav.manav147@gmail.com>
2022-02-20 18:37:11 +0530

    apicall

--- apidocument.md
+++ apidocument.md
@@ -1,3 +1,79 @@
-Page 1: List of city
-Local - http://localhost:6700/location
-Live - 
+***Page 1 ***
+List of City 
+>    Local - http://localhost:6700/location
+>    Live - 
+
+List of Restaurant
+>    Local - http://localhost:6700/restaurantdata
+>    Live - 
+restaurant with respect to city
+>    Local - http://localhost:6700/restaurantdata?state_id=3
+>    Live - 
+Quick Search Data (mealtype)
+>    Local - http://localhost:6700/mealtype
+>    Live -
+
+
+***Page 2 ***
+Restaurant with respect to state and mealtype
+> Local: - http://localhost:6700/restaurantdata?state_id=4&meal_id=5
+
+Restaurant with respect to Quick Search
+> Local - http://localhost:6700/restaurantdata?meal_id=5
+
+/// filter data: - ///
+cuisine filter (data with respect to cuisine and quick search)
+> Local - http://localhost:6700/filter/1?cuisine=2
+> Live - 
+
+cost filter
+> Local - http://localhost:6700/filter/2?lcost=250&hcost=500
+> Live - 
+
+data respect to cuisine and cost (cuisine filter + cost filter)
+> Local - http://localhost:6700/filter/1?lcost=250&hcost=500&cuisineId=4
+> Live - 
+
+sort sort low to high in same quickSearch
+> Local - http://localhost:6700/filter/1?lcost=250&hcost=500&cuisineId=1&sort=1
+> Live - 
+
+sort high to low in same quickSearch
+> Local - http://localhost:6700/filter/1?lcost=250&hcost=500&cuisineId=1&sort=-1
+> Live - 
+
+Paginations
+> Local - http://localhost:6700/filter/1?cuisineId=1&skip=12&limit=1
+> Live - 
+
+*** Page 3: -***
+restaurants details
+> Local - http://localhost:6700/details/3
+> Live - 
+
+Menu of that restaurants 
+> Local - http://localhost:6700/menu/1
+> Live - 
+
+page 4 : -
+menu items on user selection
+> Local - localhost:6700/menuItem 
+        [4,5,7]
+
+
+api to place order
+> Local - localhost:6700/placeOrder
+> Live - 
+
+Delete order
+> Local - localhost:6700/deleteOrder
+
+update order
+> Local - localhost:6700/updateOrder/621228313aa7a46d5cf5c7ba?status=success
+
+
+page 5 : -
+list all order 
+> Local - http://localhost:6700/orders?email=a@gmail.com or http://localhost:6700/orders
+
+
--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const MongoClient = mongo.MongoClient;
 const mongoUrl = "mongodb+srv://test:test123@cluster0.p7mvq.mongodb.net/novintern?retryWrites=true&w=majority"
 const dotenv = require('dotenv');
 dotenv.config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 let port = process.env.PORT || 8210;
 var db;
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
 
 /*Get Request */
 app.get('/',(req,res) => {
@@ -23,12 +29,174 @@ app.get('/location',(req,res) => {
     })
 })
 
-app.get('/restaurantmenu',(req,res) => {
+/*app.get('/restaurantmenu',(req,res) => {
     db.collection('restaurantmenu').find().toArray((err, result) =>{
         if(err) throw err;
         res.send(result)
     })
+})*/
+
+
+/*Restaurants as per Location */
+app.get('/restaurantdata',(req,res) => {
+    let StateId = Number(req.query.state_id)
+    let mealId = Number(req.query.meal_id)
+    let query = {};
+    if (StateId && mealId){
+        query = {"mealTypes.mealtype_id":mealId,state_id:StateId};
+    }
+    else if (StateId){
+        query = {state_id:StateId};
+    }
+    else if (mealId){
+        query = {"mealTypes.mealtype_id":mealId};
+    }
+
+    console.log(">>>>restID" ,StateId)
+    db.collection('restaurantdata').find(query).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
 })
+
+/* Filters*/
+app.get('/filter/:mealId',(req,res) => {
+    let sort = {cost:1}
+    let mealId = Number(req.params.mealId)
+    let skip = 0
+    let limit = 1000000000;
+    let cuisineId = Number(req.query.cuisine)
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost)
+    let query = {}
+    if(req.query.sort){
+        sort = {cost:req.query.sort}
+    }
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit)
+    }
+
+    if(cuisineId&lcost&hcost) {
+        query = {"cuisines.cuisine_id":cuisineId, "mealTypes.mealtype_id":mealId, $and:[{cost:{$gt:lcost, $lt:hcost}}]
+    }
+}
+
+    else if(cuisineId){
+        query = {"cuisines.cuisine_id":cuisineId,"mealTypes.mealtype_id":mealId}
+    }
+
+    else if(lcost&hcost){
+        query = {$and:[{cost:{$gt:lcost, $lt:hcost}}],"mealTypes.mealtype_id":mealId}
+    }
+
+    db.collection('restaurantdata').find(query).sort(sort).skip(skip).limit(limit).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+
+/* Get mealtype */
+app.get('/mealtype',(req,res) => {
+    db.collection('mealtype').find().toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/*Restaurant details*/
+app.get('/details/:id',(req,res) => {
+    let restID = Number(req.params.id)
+    //restID = mongo.ObjectId(req.params.id)
+    db.collection('restaurantdata').find({restaurant_id:restID}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/*Menu with respect to details*/
+app.get('/menu/:id',(req,res) => {
+    let restID = Number(req.params.id)
+    db.collection('restaurantmenu').find({restaurant_id:restID}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/*Menu on basis of USER SELCETION */
+
+//Get all Orders 
+app.get('/orders',(req,res) => {
+    let email = req.query.email
+    let query = {};
+    if (email){
+        query = {"email":email};
+    }
+
+    db.collection('order').find(query).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/* Place Order */
+app.post('/placeOrder', (req, res) => {
+    //console.log(req.body)
+    db.collection('order').insert(req.body, (err, result) =>{
+        if(err) throw err;
+        res.send('Order Added Successfully')
+    })
+})
+
+app.post('/menuItem', (req, res) => {
+    console.log(req.body)
+    db.collection('restaurantmenu').find({menu_id:{$in:req.body}}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/* Delete Orders */
+app.delete('/deleteOrder', (req, res) => {
+    db.collection('order').remove({}, (err, result)  =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+/* Update Orders */
+// app.put('/updateOrder/:id', (req, res) => {
+//     let oId = mongo.ObjectId(req.params.id)
+//     let status = req.query.status?req.query.status:'Pending'
+//     db.collection('order').updateOne(
+//         {_id: oId},
+//         {$set:{
+//             "status":status
+//         }}, (err, result) =>{
+//             if(err) throw err;
+//             res.send(`Status updated to ${status}`)
+//         })
+// })
+
+/* Update with Bank Name */
+app.put('/updateOrder/:id', (req, res) => {
+    let oId = mongo.ObjectId(req.params.id)
+    let status = req.query.status?req.query.status:'Pending'
+    db.collection('order').updateOne(
+        {_id: oId},
+        {$set:{
+            "status":status,
+            "bank_name":req.body.bank_name,
+            "bank_status":req.body.bank_status,
+        }}, (err, result) =>{
+            if(err) throw err;
+            res.send(`Status updated to ${status}`)
+        })
+})
+
+
 
 MongoClient.connect(mongoUrl, (err, connection) => {
     if(err) console.log('Error While Connecting');
